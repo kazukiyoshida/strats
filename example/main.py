a9f6c86869d21902e54422e013dc4af926f9b117
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-21 20:09:40 +0900

    refactor: update example code

--- example/main.py
+++ example/main.py
@@ -1,92 +1,44 @@
-from time import sleep, time
+import logging
 
-from prometheus_client import Gauge
+from strats_oanda.client import PricingStreamClient
 
+from strats import Strats
 from strats.core import DataWithMetrics, singletondataclass
 from strats.model import PricesData, PricesMetrics, prices_mapper
+from strats.monitor import StreamMonitor
+
+logging.basicConfig(level=logging.INFO)
 
 
 @singletondataclass
 class State:
-    usdjpy: DataWithMetrics[PricesData, PricesMetrics]
-    btcusd: DataWithMetrics[PricesData, PricesMetrics]
-    time: DataWithMetrics[str, str]
+    prices: DataWithMetrics[PricesData, PricesMetrics]
 
 
-print("==== start ====")
-
-usdjpy_prices = DataWithMetrics(
-    data=PricesData(bid=0, ask=0),
+prices = DataWithMetrics(
+    data=PricesData(
+        bid=0,
+        ask=0,
+    ),
     metrics=PricesMetrics(prefix="usdjpy"),
     mapper=prices_mapper,
 )
 
-btcusd_prices = DataWithMetrics(
-    data=PricesData(bid=0, ask=0),
-    metrics=PricesMetrics(prefix="btcusd"),
-    mapper=prices_mapper,
+state = State(
+    prices=prices,
 )
 
-t = DataWithMetrics(
-    data=int(time()),
-    metrics=Gauge("time", ""),
-    mapper=lambda d, m: m.set(d),
+prices_monitor = StreamMonitor(
+    state=state,
+    client=PricingStreamClient(instruments=["USD_JPY"]),
+    handler=None,
 )
 
-s = State(
-    usdjpy=usdjpy_prices,
-    btcusd=btcusd_prices,
-    time=t,
+app = Strats(
+    state=state,
+    monitors={
+        "prices_monitor": prices_monitor,
+    },
 )
 
-print("s: ", s)
-
-print("======= time ==============")
-
-print("------- before")
-print("s.time:             ", s.time)
-print("s.time.data:        ", s.time.data)
-print("s.time.metrics:     ", s.time.metrics)
-
-sleep(2)
-s.time.data = int(time())
-
-print("------- after")
-print("s.time:             ", s.time)
-print("s.time.data:        ", s.time.data)
-print("s.time.metrics:     ", s.time.metrics)
-
-print("======= usdjpy ==============")
-
-print("------- before")
-print("s.usdjpy:             ", s.usdjpy)
-print("s.usdjpy.data:        ", s.usdjpy.data)
-print("s.usdjpy.metrics:     ", s.usdjpy.metrics)
-print("s.usdjpy.metrics.bid: ", s.usdjpy.metrics.bid._value.get())
-
-s.usdjpy.data = PricesData(bid=100, ask=102)
-
-print("------- after")
-print("s.usdjpy:             ", s.usdjpy)
-print("s.usdjpy.data:        ", s.usdjpy.data)
-print("s.usdjpy.metrics:     ", s.usdjpy.metrics)
-print("s.usdjpy.metrics.bid: ", s.usdjpy.metrics.bid._value.get())
-
-print("======= btcusd ==============")
-
-print("------- before")
-print("s.btcusd:             ", s.btcusd)
-print("s.btcusd.data:        ", s.btcusd.data)
-print("s.btcusd.metrics:     ", s.btcusd.metrics)
-print("s.btcusd.metrics.bid: ", s.btcusd.metrics.bid._value.get())
-
-s.btcusd.data = PricesData(bid=900, ask=902)
-
-print("------- after")
-print("s.btcusd:             ", s.btcusd)
-print("s.btcusd.data:        ", s.btcusd.data)
-print("s.btcusd.metrics:     ", s.btcusd.metrics)
-print("s.btcusd.metrics.bid: ", s.btcusd.metrics.bid)
-print("s.btcusd.metrics.bid: ", s.btcusd.metrics.bid._value.get())
-
-print("==== end ====")
+app.serve()
