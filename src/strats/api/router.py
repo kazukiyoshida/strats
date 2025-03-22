0af01f7292f0388741d3fbbabdf6c1e7bd648b83
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-22 12:42:46 +0900

    feat: use FastAPI, uvloop and uvicorn

--- src/strats/api/router.py
+++ src/strats/api/router.py
@@ -1,49 +1,44 @@
 import logging
 
-from flask import Blueprint, current_app, jsonify
+from fastapi import APIRouter, Depends
 from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 
 from strats.core.kernel import Kernel
 
 logger = logging.getLogger(__name__)
-router = Blueprint("router", __name__)
+
+router = APIRouter()
 
 
 def get_kernel() -> Kernel:
-    kernel = current_app.config["kernel"]
-    if kernel is None:
-        raise Exception("kernel was not found in the Strats")
-    return kernel
+    raise NotImplementedError("get_kernel is not yet bound")
 
 
-@router.route("/healthz", methods=["GET"])
+@router.get("/healthz")
 def healthz():
     return "ok"
 
 
-@router.route("/metrics", methods=["GET"])
+@router.get("/metrics")
 def metrics():
     return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
 
 
-@router.route("/monitors", methods=["GET"])
-def get_monitors():
-    kernel = get_kernel()
-    return jsonify(response_monitors_info(kernel))
+@router.get("/monitors")
+def get_monitors(kernel: Kernel = Depends(get_kernel)):
+    return response_monitors_info(kernel)
 
 
-@router.route("/monitors/start", methods=["POST"])
-def start_monitors():
-    kernel = get_kernel()
+@router.post("/monitors/start")
+def start_monitors(kernel: Kernel = Depends(get_kernel)):
     kernel.start_monitors()
-    return jsonify(response_monitors_info(kernel))
+    return response_monitors_info(kernel)
 
 
-@router.route("/monitors/stop", methods=["POST"])
-def stop_monitors():
-    kernel = get_kernel()
+@router.post("/monitors/stop")
+def stop_monitors(kernel: Kernel = Depends(get_kernel)):
     kernel.stop_monitors()
-    return jsonify(response_monitors_info(kernel))
+    return response_monitors_info(kernel)
 
 
 def response_monitors_info(kernel):
--- src/strats/api/server.py
+++ src/strats/api/server.py
@@ -1,11 +1,14 @@
 import logging
 
-import waitress
-from flask import Flask
+import uvicorn
+import uvloop
+from fastapi import FastAPI
 
 from strats.core.kernel import Kernel
 
-from .router import router
+from .router import get_kernel, router
+
+uvloop.install()
 
 BANNER = r"""
  _______ _______  ______ _______ _______ _______
@@ -16,10 +19,17 @@ BANNER = r"""
 logger = logging.getLogger(__name__)
 
 
+def create_get_kernel(kernel):
+    def get_kernel():
+        return kernel
+
+    return get_kernel
+
+
 class Strats(Kernel):
     def serve(self, host="0.0.0.0", port=8000):
-        app = Flask(__name__)
-        app.config["kernel"] = self
-        app.register_blueprint(router)
+        app = FastAPI()
+        app.include_router(router)
+        app.dependency_overrides[get_kernel] = create_get_kernel(self)
         logger.info(BANNER)
-        waitress.serve(app, host=host, port=port)
+        uvicorn.run(app=app)
