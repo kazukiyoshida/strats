ea0dc8363f9b0af72dc2f00ba3a0aab2dd9c1cb3
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-24 22:21:16 +0900

    fix: fix the metrics API format

--- src/strats/api/router.py
+++ src/strats/api/router.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 
 from strats.core.kernel import Kernel
@@ -21,7 +21,8 @@ def healthz():
 
 @router.get("/metrics")
 def metrics():
-    return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
+    data = generate_latest()
+    return Response(content=data, media_type=CONTENT_TYPE_LATEST)
 
 
 @router.get("/monitors")
