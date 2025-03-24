804fe4dbfb3e4d53cde16a3b12f35bce61393443
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-25 01:03:43 +0900

    chore: update example script

--- example/main.py
+++ example/main.py
@@ -1,21 +1,29 @@
+import argparse
 import logging
+import sys
+from dataclasses import dataclass
 
 import strats_oanda
-from strats_oanda.client import PricingStreamClient
+from strats_oanda.client import PricingStreamClient, TransactionClient
 from strats_oanda.converter import client_price_to_prices
 from strats_oanda.model import ClientPrice
 
-from strats import Strats
-from strats.core import DataWithMetrics, singletondataclass
+from strats import DataWithMetrics, Strats
 from strats.model import PricesData, PricesMetrics
 from strats.monitor import StreamMonitor
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-strats_oanda.basic_config(use_file=True, file_path=".strats_oanda.yaml")
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", type=str, default="local", help="local|practice|prod")
+    opts = parser.parse_args(argv)
+    return opts
 
 
-@singletondataclass
+@dataclass
 class State:
     prices: DataWithMetrics[PricesData, PricesMetrics]
 
@@ -25,25 +33,49 @@ class State:
             self.prices.data = data
 
 
-state = State(
-    prices=DataWithMetrics(
-        data=PricesData(
-            bid=0,
-            ask=0,
+def main(argv=sys.argv[1:]):
+    opts = parse_args(argv)
+
+    if opts.env == "prod":
+        file_path = ".strats_oanda_prod.yaml"
+    elif opts.env == "practice":
+        file_path = ".strats_oanda_practice.yaml"
+    else:
+        file_path = ".strats_oanda.yaml"
+
+    logger.info(f"use {file_path}")
+    strats_oanda.basic_config(use_file=True, file_path=file_path)
+
+    state = State(
+        prices=DataWithMetrics(
+            data=PricesData(
+                bid=0,
+                ask=0,
+            ),
+            metrics=PricesMetrics(prefix="usdjpy"),
         ),
-        metrics=PricesMetrics(prefix="usdjpy"),
-    ),
-)
+    )
 
-prices_monitor = StreamMonitor[State, ClientPrice](
-    state=state,
-    client=PricingStreamClient(instruments=["USD_JPY"]),
-    handler=lambda state, msg: state.update_prices(msg),
-)
+    prices_monitor = StreamMonitor[State, ClientPrice](
+        state=state,
+        client=PricingStreamClient(instruments=["USD_JPY"]),
+        handler=lambda state, msg: state.update_prices(msg),
+    )
 
-Strats(
-    state=state,
-    monitors={
-        "prices_monitor": prices_monitor,
-    },
-).serve()
+    transaction_monitor = StreamMonitor[State, ClientPrice](
+        state=state,
+        client=TransactionClient(),
+        handler=lambda state, msg: print(msg),
+    )
+
+    Strats(
+        state=state,
+        monitors={
+            "prices_monitor": prices_monitor,
+            "transaction_monitor": transaction_monitor,
+        },
+    ).serve()
+
+
+if __name__ == "__main__":
+    main()
