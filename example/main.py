cbf1cbbb5083bda53300781c89b1eadb157c27a9
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-24 22:45:51 +0900

    feat: update example main.py

--- example/main.py
+++ example/main.py
@@ -2,10 +2,12 @@ import logging
 
 import strats_oanda
 from strats_oanda.client import PricingStreamClient
+from strats_oanda.converter import client_price_to_prices
+from strats_oanda.model import ClientPrice
 
 from strats import Strats
 from strats.core import DataWithMetrics, singletondataclass
-from strats.model import PricesData, PricesMetrics, prices_mapper
+from strats.model import PricesData, PricesMetrics
 from strats.monitor import StreamMonitor
 
 logging.basicConfig(level=logging.INFO)
@@ -17,24 +19,26 @@ strats_oanda.basic_config(use_file=True, file_path=".strats_oanda.yaml")
 class State:
     prices: DataWithMetrics[PricesData, PricesMetrics]
 
+    def update_prices(self, msg: ClientPrice):
+        data = client_price_to_prices(msg)
+        if data is not None:
+            self.prices.data = data
 
-prices = DataWithMetrics(
-    data=PricesData(
-        bid=0,
-        ask=0,
-    ),
-    metrics=PricesMetrics(prefix="usdjpy"),
-    mapper=prices_mapper,
-)
 
 state = State(
-    prices=prices,
+    prices=DataWithMetrics(
+        data=PricesData(
+            bid=0,
+            ask=0,
+        ),
+        metrics=PricesMetrics(prefix="usdjpy"),
+    ),
 )
 
-prices_monitor = StreamMonitor(
+prices_monitor = StreamMonitor[State, ClientPrice](
     state=state,
     client=PricingStreamClient(instruments=["USD_JPY"]),
-    handler=lambda state, msg: print(msg),
+    handler=lambda state, msg: state.update_prices(msg),
 )
 
 Strats(
