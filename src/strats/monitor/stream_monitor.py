56f9510755cda53bac4918fa593449ee340df918
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-25 01:02:58 +0900

    fix: fix the error handling in StreamMonitor

--- src/strats/monitor/stream_monitor.py
+++ src/strats/monitor/stream_monitor.py
@@ -39,34 +39,37 @@ class StreamMonitor(Monitor, Generic[S, D]):
 
         client = self.client.stream(stop_event)
 
-        try:
-            while not stop_event.is_set():
-                # 一時的な task を開始
-                data_task = asyncio.create_task(client.__anext__())
-                stop_task = asyncio.create_task(stop_event.wait(), name="tmp-stop-event")
+        while not stop_event.is_set():
+            # 一時的な task を開始
+            data_task = asyncio.create_task(client.__anext__())
+            stop_task = asyncio.create_task(stop_event.wait(), name="tmp-stop-event")
 
-                done, pending = await asyncio.wait(
-                    [data_task, stop_task],
-                    return_when=asyncio.FIRST_COMPLETED,
-                )
+            done, pending = await asyncio.wait(
+                [data_task, stop_task],
+                return_when=asyncio.FIRST_COMPLETED,
+            )
 
-                # 不必要になった一時的な task は終了
-                for task in pending:
-                    task.cancel()
+            # 不必要になった一時的な task は終了
+            for task in pending:
+                task.cancel()
 
-                if stop_task in done:
-                    logger.info(f"monitor={name}: stop_event received")
+            if stop_task in done:
+                logger.info(f"monitor={name}: stop_event received")
+                break
+
+            if data_task in done:
+                # the body of an async generator function does not execute
+                # until the first `__anext__()` call. Therefore, exceptions
+                # raised before the first `yield` are not visible until iteration begins.
+                try:
+                    item = data_task.result()
+                except StopAsyncIteration:
+                    logger.info(f"{name}: streaming client stopped")
                     break
+                except Exception as e:
+                    logger.error(f"{name}: streaming client got error: {e}")
+                    break
+                self.handler(self.state, item)
 
-                if data_task in done:
-                    try:
-                        item = data_task.result()
-                        self.handler(self.state, item)
-                    except StopAsyncIteration:
-                        # client が終了した場合
-                        logger.info(f"{name}: generator exhausted")
-                        break
-
-        finally:
-            await client.aclose()
-            logger.info(f"{name}: gracefully stopped.")
+        await client.aclose()
+        logger.info(f"{name}: stopped")
