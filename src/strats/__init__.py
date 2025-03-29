fc667c76189b93b9b0af23dcb40d38c9382fdb47
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-30 00:37:31 +0900

    chore: expose Monitor

--- src/strats/__init__.py
+++ src/strats/__init__.py
@@ -2,4 +2,5 @@ __version__ = "0.1.0"
 
 from .api import Strats as Strats
 from .core import DataWithMetrics as DataWithMetrics
+from .monitor import Monitor as Monitor
 from .strategy import Strategy as Strategy
