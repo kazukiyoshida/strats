0f6ba7301ebf4fbfe9fabef1981c8721179767c9
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-31 06:03:24 +0900

    feat: sync __version__ and version in pyproject.toml

--- src/strats/__init__.py
+++ src/strats/__init__.py
@@ -1,4 +1,6 @@
-__version__ = "0.1.0"
+from importlib.metadata import version
+
+__version__ = version(__package__)
 
 from .api import Strats as Strats
 from .core import DataWithMetrics as DataWithMetrics
