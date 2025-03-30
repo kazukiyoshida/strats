b54cdc3db2480a4f1929c58d3243a552a465d7ff
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-31 06:04:24 +0900

    feat: delete singleton_dataclass and data_with_metrics

--- src/strats/__init__.py
+++ src/strats/__init__.py
@@ -3,6 +3,5 @@ from importlib.metadata import version
 __version__ = version(__package__)
 
 from .api import Strats as Strats
-from .core import DataWithMetrics as DataWithMetrics
 from .monitor import Monitor as Monitor
 from .strategy import Strategy as Strategy
--- src/strats/core/__init__.py
+++
@@ -1,2 +0,0 @@
-from .data_with_metrics import DataWithMetrics as DataWithMetrics
-from .singleton_dataclass import singletondataclass as singletondataclass
