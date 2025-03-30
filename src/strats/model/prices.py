1a2aff38d2e7baebf80b57d35a1235c18dbf876d
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-03-31 06:06:57 +0900

    feat: decompose PricesMetrics

--- src/strats/model/prices.py
+++ src/strats/model/prices.py
@@ -1,27 +1,25 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from decimal import Decimal
-from typing import Optional
 
 from prometheus_client import Counter, Gauge
 
 
 @dataclass
 class PricesData:
-    bid: Optional[Decimal] = field(default=None)
-    ask: Optional[Decimal] = field(default=None)
+    bid: Decimal = Decimal("0")
+    ask: Decimal = Decimal("0")
 
 
 class PricesMetrics:
-    def __init__(self, prefix: Optional[str] = None):
-        s = "" if prefix is None else f"{prefix}_"
-        self.bid = Gauge(f"{s}prices_bid", "")
-        self.ask = Gauge(f"{s}prices_ask", "")
-        self.spread = Gauge(f"{s}prices_spread", "")
-        self.update_count = Counter(f"{s}prices_update_count", "")
+    def __init__(self, prefix: str):
+        self.bid = Gauge(f"{prefix}_prices_bid", "")
+        self.ask = Gauge(f"{prefix}_prices_ask", "")
+        self.spread = Gauge(f"{prefix}_prices_spread", "")
+        self.update_count = Counter(f"{prefix}_prices_update_count", "")
 
-    def default_data_mapper(self, d: PricesData):
-        if d.bid is not None and d.ask is not None:
-            self.bid.set(float(d.bid))
-            self.ask.set(float(d.ask))
-            self.spread.set(float(d.ask - d.bid))
-            self.update_count.inc()
+
+def prices_data_to_prices_metrics(data: PricesData, metrics: PricesMetrics):
+    metrics.bid.set(float(data.bid))
+    metrics.ask.set(float(data.ask))
+    metrics.spread.set(float(data.ask - data.bid))
+    metrics.update_count.inc()
