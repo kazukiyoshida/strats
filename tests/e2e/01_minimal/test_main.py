dac35129ff3f1db64b214b0ea300cbdfab10bdf9
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2025-04-06 01:02:00 +0900

    refactor: refactor e2e/01_minimal using fixture

--- tests/e2e/01_minimal/test_main.py
+++ tests/e2e/01_minimal/test_main.py
@@ -3,67 +3,74 @@ import sys
 import time
 from urllib.parse import urljoin
 
+import pytest
 import requests
 
 BASE_URL = "http://localhost:8000"
 
 
-def test_01_minimal():
+@pytest.fixture(scope="function")
+def app_process():
     proc = subprocess.Popen(
-        ["python", "tests/e2e/01_minimal/main.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        ["python", "tests/e2e/01_minimal/main.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
+    # wait the application is running
     time.sleep(1)
 
-    try:
-        if proc.poll() is not None:
-            stdout, stderr = proc.communicate()
-            print("[STDOUT]", stdout.decode(), file=sys.stderr)
-            print("[STDERR]", stderr.decode(), file=sys.stderr)
-            raise RuntimeError("Application process exited early")
+    if proc.poll() is not None:
+        stdout, stderr = proc.communicate()
+        print("[STDOUT]", stdout.decode(), file=sys.stderr)
+        print("[STDERR]", stderr.decode(), file=sys.stderr)
+        raise RuntimeError("Application process exited early")
 
-        # >> healthz, metrics
+    yield proc
 
-        res = requests.get(urljoin(BASE_URL, "/healthz"))
-        assert res.status_code == 200
-        assert res.json() == "ok"
+    proc.terminate()
+    proc.wait()
 
-        res = requests.get(urljoin(BASE_URL, "/metrics"))
-        assert res.status_code == 200
 
-        # >> strategy
+def test_minimal(app_process):
+    # >> healthz, metrics
 
-        res = requests.get(urljoin(BASE_URL, "/strategy"))
-        expect = {"is_configured": False, "is_running": False}
-        assert res.status_code == 200
-        assert res.json() == expect
+    res = requests.get(urljoin(BASE_URL, "/healthz"))
+    assert res.status_code == 200
+    assert res.json() == "ok"
 
-        res = requests.post(urljoin(BASE_URL, "/strategy/start"))
-        expect = {"detail": "Missing strategy configuration"}
-        assert res.status_code == 400
-        assert res.json() == expect
+    res = requests.get(urljoin(BASE_URL, "/metrics"))
+    assert res.status_code == 200
 
-        res = requests.post(urljoin(BASE_URL, "/strategy/stop"))
-        expect = {"detail": "Missing strategy configuration"}
-        assert res.status_code == 400
-        assert res.json() == expect
+    # >> strategy
 
-        # >> monitors
+    res = requests.get(urljoin(BASE_URL, "/strategy"))
+    expect = {"is_configured": False, "is_running": False}
+    assert res.status_code == 200
+    assert res.json() == expect
 
-        res = requests.get(urljoin(BASE_URL, "/monitors"))
-        expect = {"is_configured": False}
-        assert res.status_code == 200
-        assert res.json() == expect
+    res = requests.post(urljoin(BASE_URL, "/strategy/start"))
+    expect = {"detail": "Missing strategy configuration"}
+    assert res.status_code == 400
+    assert res.json() == expect
 
-        res = requests.post(urljoin(BASE_URL, "/monitors/start"))
-        expect = {"detail": "Missing monitors configuration"}
-        assert res.status_code == 400
-        assert res.json() == expect
+    res = requests.post(urljoin(BASE_URL, "/strategy/stop"))
+    expect = {"detail": "Missing strategy configuration"}
+    assert res.status_code == 400
+    assert res.json() == expect
 
-        res = requests.post(urljoin(BASE_URL, "/monitors/stop"))
-        expect = {"detail": "Missing monitors configuration"}
-        assert res.status_code == 400
-        assert res.json() == expect
+    # >> monitors
 
-    finally:
-        proc.terminate()
-        proc.wait()
+    res = requests.get(urljoin(BASE_URL, "/monitors"))
+    expect = {"is_configured": False}
+    assert res.status_code == 200
+    assert res.json() == expect
+
+    res = requests.post(urljoin(BASE_URL, "/monitors/start"))
+    expect = {"detail": "Missing monitors configuration"}
+    assert res.status_code == 400
+    assert res.json() == expect
+
+    res = requests.post(urljoin(BASE_URL, "/monitors/stop"))
+    expect = {"detail": "Missing monitors configuration"}
+    assert res.status_code == 400
+    assert res.json() == expect
